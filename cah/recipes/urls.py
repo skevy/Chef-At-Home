3dd63d762c16a57ccbe02750ed4671955573de48
Nick Hudkins <nickhudkins@jess3.com>
2011-04-06 02:14:32 -0400

    ...

--- cah/recipes/urls.py
+++ cah/recipes/urls.py
@@ -6,4 +6,5 @@ from django.http import HttpResponse
 urlpatterns = patterns('cah.recipes.views',
     url(r'^$', 'index'),
     url(r'^2/$', 'detail'),
+    url(r'^add/$', 'add'),
 )
--- cah/recipes/views.py
+++ cah/recipes/views.py
@@ -5,3 +5,20 @@ def index(request):
     
 def detail(request):
     return render(request, "recipes/detail.html", {})
+    
+def add(request):
+    
+    first_name = "Adam"
+    last_name = "Miskiewicz"
+    
+    user = {
+        'first_name': first_name,
+        'last_name': last_name,
+    }
+    
+    context = {
+    'initial_ingredients':['ingredient1','ingredient2','ingredient3'],
+    'user': user
+    
+    }
+    return render(request, "recipes/add_recipe.html", context)
